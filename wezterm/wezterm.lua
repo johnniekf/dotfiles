79c95693f89a9d19b9e4de48c31650f24521c0d0
Johnnie <johnnie.kf@gmail.com>
2025-07-05 13:19:46 +0100

    Add Wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+
+return {
+	color_scheme = "Catppuccin Mocha",
+	enable_wayland = true,
+	window_background_opacity = 0,
+	-- window_blur = true,
+	kde_window_background_blur = true,
+	font = wezterm.font("JetBrains Mono Nerd Font", {weight = "Medium"}),
+}
