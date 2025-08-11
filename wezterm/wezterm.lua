aa01e89777e812e1588a13a5d3d373bfbdb0fb33
Johnnie <johnnie.kf@gmail.com>
2025-08-11 13:58:54 +0100

    updates to Wez and nvim

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,40 @@
-local wezterm = require 'wezterm'
+-- local wezterm = require 'wezterm'
+-- local act = wezterm.action
+-- return {
+-- 	color_scheme = "Catppuccin Mocha",
+-- 	enable_wayland = true,
+-- 	window_background_opacity = 0,
+-- 	-- window_blur = true,
+-- 	kde_window_background_blur = true,
+-- 	-- background = 'black',
+-- 	window_background_opacity = 0.7,	
+-- 	font = wezterm.font("JetBrains Mono Nerd Font", {weight = "Medium"})
+-- }
 
-return {
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+local config = {
 	color_scheme = "Catppuccin Mocha",
 	enable_wayland = true,
-	window_background_opacity = 0,
-	-- window_blur = true,
+	window_background_opacity = 0.7, -- Set final opacity
 	kde_window_background_blur = true,
-	font = wezterm.font("JetBrains Mono Nerd Font", {weight = "Medium"}),
+	font = wezterm.font("JetBrains Mono Nerd Font", { weight = "Medium" }),
+	mouse_bindings = {
+		{
+			event = { Down = { streak = 1, button = "Right" } },
+			mods = "NONE",
+			action = wezterm.action_callback(function(window, pane)
+				local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+				if has_selection then
+					window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+					window:perform_action(act.ClearSelection, pane)
+				else
+					window:perform_action(act.PasteFrom("Clipboard"), pane)
+				end
+			end),
+		},
+	},
 }
+
+return config
